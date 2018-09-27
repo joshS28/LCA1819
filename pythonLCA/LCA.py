724349484a8223b4163c803391a7296d74aa8bb1
Joshua Stafford <Josh@Joshuas-MacBook-Pro.local>
2018-09-27 10:37:12 +0100

    Updated all python, added coverage checker and all tests complete and correct, coverage 100% and runs with html page in html folder (index.html) shows coverage and all lines ran or missed, 0 missed in this case, tests every combination

--- pythonLCA/LCA.py
+++ pythonLCA/LCA.py
@@ -1,88 +1,66 @@
 
-""" Program to find LCA of n1 and n2 using one traversal of 
- Binary tree 
-It handles all cases even when n1 or n2 is not there in tree 
-"""
-  
-# A binary tree node 
+import unittest 
+
 class Node: 
-  
-    # Constructor to create a new node 
+    # Constructor to create a new binary node 
     def __init__(self, key): 
-        self.key = key  
+        self.key =  key 
         self.left = None
         self.right = None
   
-# This function retturn pointer to LCA of two given values 
-# n1 and n2  
-# v1 is set as true by this function if n1 is found 
-# v2 is set as true by this function if n2 is found 
-def findLCAUtil(root, n1, n2, v): 
-      
-    # Base Case 
-    if root is None: 
-        return None
+# Finds the path from root node to given root of the tree. 
+# Stores the path in a list path[], returns true if path  
+# exists otherwise false 
+def findPath( root, path, k): 
   
-    # IF either n1 or n2 matches ith root's key, report 
-    # the presence by setting v1 or v2 as true and return 
-    # root (Note that if a key is ancestor of other, then  
-    # the ancestor key becomes LCA) 
-    if root.key == n1 : 
-        v[0] = True
-        return root 
-  
-    if root.key == n2: 
-        v[1] = True
-        return root 
-  
-    # Look for keys in left and right subtree 
-    left_lca = findLCAUtil(root.left, n1, n2, v) 
-    right_lca = findLCAUtil(root.right, n1, n2, v) 
-  
-    # If both of the above calls return Non-NULL, then one key 
-    # is present in once subtree and other is present in other, 
-    # So this node is the LCA 
-    if left_lca and right_lca: 
-        return root  
-  
-    # Otherwise check if left subtree or right subtree is LCA 
-    return left_lca if left_lca is not None else right_lca 
-  
-  
-def find(root, k): 
-      
-    # Base Case 
+    # Baes Case 
     if root is None: 
         return False
-      
-    # If key is present at root, or if left subtree or right 
-    # subtree , return true 
-    if (root.key == k or find(root.left, k) or
-        find(root.right, k)): 
+  
+    # Store this node is path vector. The node will be 
+    # removed if not in path from root to k 
+    path.append(root.key) 
+  
+    # See if the k is same as root's key 
+    if root.key == k : 
         return True
-      
-    # Else return false 
+  
+    # Check if k is found in left or right sub-tree 
+    if ((root.left != None and findPath(root.left, path, k)) or
+            (root.right!= None and findPath(root.right, path, k))): 
+        return True 
+  
+    # If not present in subtree rooted with root, remove 
+    # root from path and return False 
+       
+    path.pop() 
     return False
   
-# This function returns LCA of n1 and n2 onlue if both 
-# n1 and n2 are present in tree, otherwise returns None 
+# Returns LCA if node n1 , n2 are present in the given 
+# binary tre otherwise return -1 
 def findLCA(root, n1, n2): 
-      
-    # Initialize n1 and n2 as not visited 
-    v = [False, False] 
   
-    # Find lac of n1 and n2 using the technique discussed above 
-    lca = findLCAUtil(root, n1, n2, v) 
+    # To store paths to n1 and n2 fromthe root 
+    path1 = [] 
+    path2 = [] 
   
-    # Returns LCA only if both n1 and n2 are present in tree 
-    if (v[0] and v[1] or v[0] and find(lca, n2) or v[1] and 
-        find(lca, n1)): 
-        return lca 
+    # Find paths from root to n1 and root to n2. 
+    # If either n1 or n2 is not present , return -1  
+    if (not findPath(root, path1, n1) or not findPath(root, path2, n2)): 
+        return -1 
   
-    # Else return None 
-    return None
+    # Compare the paths to get the first different value 
+    i = 0 
+    while(i < len(path1) and i < len(path2)): 
+        if path1[i] != path2[i]: 
+            break
+        i += 1
+    return path1[i-1] 
   
-# Driver program to test above function 
+  
+# Driver program to test above function
+# Let's create the Binary Tree shown in above diagram 
+'''
 root = Node(1) 
 root.left = Node(2) 
 root.right = Node(3) 
@@ -91,19 +69,89 @@ root.left.right = Node(5)
 root.right.left = Node(6) 
 root.right.right = Node(7) 
   
-lca = findLCA(root, 4, 5) 
-
-"""
-if lca is not None: 
-    print "LCA(4, 5) = ", lca.key 
-else : 
-    print "Keys are not present"
+print "LCA(4, 5) = %d" %(findLCA(root, 4, 5,)) 
+print "LCA(4, 6) = %d" %(findLCA(root, 4, 6)) 
+print "LCA(3, 4) = %d" %(findLCA(root,3,4)) 
+print "LCA(2, 4) = %d" %(findLCA(root,2, 4)) '''
   
-lca = findLCA(root, 4, 10) 
-if lca is not None: 
-    print "LCA(4,10) = ", lca.key 
-else: 
-    print "Keys are not present"
-"""
+# This code is contributed by Nikhil Kumar Singh(nickzuck_007)
+
+class LearningCase(unittest.TestCase):
+
+    def test_testing_with_two_nodes(self):
+        root = Node(1) 
+        root.left = Node(2)
+        root.right = Node(3)
+#                 1
+#                / \
+#               2   3
+        self.assertEqual(findLCA(root,2,3), 1)
+
+
+    def test_testing_no_path(self):
+        root = Node(1) 
+        root.left = Node(2)
+        root.right = Node(3)
+#                 1
+#                / \
+#               2   3
+        self.assertEqual(findLCA(root,2,4), -1) 
+
+    def test_testing_root_to_itself(self):
+        root = Node(1) 
+#                 1
+#                / \
+        self.assertEqual(findLCA(root,1,1), 1)
+
+    def test_medium_tree(self):
+        root = Node(1) 
+        root.left = Node(2)
+        root.right = Node(3)
+        root.left.left = Node(5)
+        root.left.left.left = Node(7)
+        root.left.right = Node(10)
+#                 1
+#                / \
+#               2   3
+#              / \
+#             5   10
+#            /
+#           7
+        self.assertEqual(findLCA(root,10,7), 2)
+
+    def test_testing_root_to_itself2(self):
+        root = None
+#                 None
+#                /    \
+        self.assertEqual(findLCA(root,0,0), -1)
+
+    def test_large_tree(self):
+        root = Node(1) 
+        root.left = Node(2)
+        root.right = Node(3)
+        root.left.left = Node(5)
+        root.left.left.left = Node(7)
+        root.left.right = Node(10)
+        root.right.right = Node(11)
+        root.right.right.right = Node(13)
+        root.right.right.left = Node(15)
+        root.left.right.left = Node(8)
+#                 1
+#                / \
+#               2   3
+#              / \   \
+#             5   10  11
+#            /   /   /  \
+#           7   8  15    13
+        self.assertEqual(findLCA(root,15,8), 1)
+        self.assertEqual(findLCA(root,7,8), 2)
+        self.assertEqual(findLCA(root,13,2), 1)
+        self.assertEqual(findLCA(root,10,11), 1)
+        self.assertEqual(findLCA(root,15,13), 11)
+
+
+if __name__ == "__main__":
+    unittest.main()
+
   
 # This code is contributed by Nikhil Kumar Singh(nickzuck_007) 
