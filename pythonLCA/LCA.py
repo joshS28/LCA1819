3e95f96f26f610279c11c4d0e1145b20b8ac6d7c
Joshua Stafford <Josh@Joshuas-MacBook-Pro.local>
2018-09-27 10:39:21 +0100

    Added and deleted a few needless lines of code

--- pythonLCA/LCA.py
+++ pythonLCA/LCA.py
@@ -56,25 +56,6 @@ def findLCA(root, n1, n2):
             break
         i += 1
     return path1[i-1] 
-  
-  
-# Driver program to test above function
-# Let's create the Binary Tree shown in above diagram 
-'''
-root = Node(1) 
-root.left = Node(2) 
-root.right = Node(3) 
-root.left.left = Node(4) 
-root.left.right = Node(5) 
-root.right.left = Node(6) 
-root.right.right = Node(7) 
-  
-print "LCA(4, 5) = %d" %(findLCA(root, 4, 5,)) 
-print "LCA(4, 6) = %d" %(findLCA(root, 4, 6)) 
-print "LCA(3, 4) = %d" %(findLCA(root,3,4)) 
-print "LCA(2, 4) = %d" %(findLCA(root,2, 4)) '''
-  
-# This code is contributed by Nikhil Kumar Singh(nickzuck_007)
 
 class LearningCase(unittest.TestCase):
 
@@ -152,6 +133,3 @@ class LearningCase(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
-
-  
-# This code is contributed by Nikhil Kumar Singh(nickzuck_007) 
